097bae3d0809703dc474ce06f686dc2239c84ae4
Lucas Bomfim <lucassbomfimm@gmail.com>
2024-11-10 18:33:40 -0300

    Added login and register logic + jobs CRUD

--- frontend/src/pages/login/Login.js
+++ frontend/src/pages/login/Login.js
@@ -1,56 +1,184 @@
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Grid from '@mui/material/Grid2';
-import { Button } from '@mui/material';
-import './Login.css';
+import React, { useState } from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Grid from "@mui/material/Grid";
+import { Button } from "@mui/material";
+import "./Login.css";
+import { AxiosError } from "axios";
+import httpClient from "../../shared/http-client/http-client";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [emailInput, setEmailInput] = useState("");
+  const [passwordInput, setPasswordInput] = useState("");
+  const [nameInput, setNameInput] = useState("");
+  const [cpfInput, setCpfInput] = useState("");
+  const [registerEmailInput, setRegisterEmailInput] = useState("");
+  const [registerPasswordInput, setRegisterPasswordInput] = useState("");
+  const [formValid, setFormValid] = useState(null);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    if (!emailInput) {
+      setFormValid("Email inválido.");
+      return;
+    }
+
+    if (passwordInput.length < 5) {
+      setFormValid("A senha precisa ter pelo menos 5 caracteres.");
+      return;
+    }
+
+    const loginUserPayload = {
+      email: emailInput,
+      password: passwordInput,
+    };
+
+    try {
+      const result = await httpClient.post("login", loginUserPayload);
+      const { token } = result.data;
+
+      localStorage.setItem("token", token);
+      setFormValid(null);
+
+      navigate('/home');
+    } catch (e) {
+      if (e instanceof AxiosError) {
+        const error = e.response?.data;
+        setFormValid(error?.message || "Erro no login");
+      }
+    }
+  };
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    if (
+      !nameInput ||
+      !cpfInput ||
+      !registerEmailInput ||
+      registerPasswordInput.length < 5
+    ) {
+      setFormValid("Por favor, preencha todos os campos corretamente.");
+      return;
+    }
+
+    const registerUserPayload = {
+      username: nameInput,
+      cpf: cpfInput,
+      email: registerEmailInput,
+      password: registerPasswordInput,
+    };
+
+    try {
+      await httpClient.post("register", registerUserPayload);
+
+      setFormValid("Cadastro realizado com sucesso! Faça login.");
+    } catch (e) {
+      if (e instanceof AxiosError) {
+        const error = e.response?.data;
+        setFormValid(error?.message || "Erro no cadastro");
+      }
+    }
+  };
+
   return (
-    <div className='background-login'>
-      <hr/>
+    <div className="background-login">
+      <hr />
       <Grid container spacing={4} justifyContent="center" alignItems="center">
-        <Grid size={4}>
-          <Card className='card'>
+        {/* Login Card */}
+        <Grid item xs={12} md={4}>
+          <Card className="card">
             <CardContent>
               <h1>LOGIN</h1>
-              <br/><br/>
               <label>E-mail</label>
-              <br/><br/>
-              <input type='text' placeholder='E-mail'/> 
-              <br/><br/>
+              <input
+                type="text"
+                placeholder="E-mail"
+                value={emailInput}
+                onChange={(e) => setEmailInput(e.target.value)}
+              />
+              <br />
+              <br />
               <label>Senha</label>
-              <br/><br/>
-              <input type='text' placeholder='Senha'/>
-            </CardContent><br/>
-            <Button variant='contained' color='secondary'>Entrar</Button>
+              <input
+                type="password"
+                placeholder="Senha"
+                value={passwordInput}
+                onChange={(e) => setPasswordInput(e.target.value)}
+              />
+              {formValid && <p className="error-message">{formValid}</p>}
+            </CardContent>
+            <Button variant="contained" color="secondary" onClick={handleLogin}>
+              Entrar
+            </Button>
           </Card>
         </Grid>
-        <Grid size={4}>
-          <Card className='card'>
+
+        {/* Register Card */}
+        <Grid item xs={12} md={4}>
+          <Card className="card">
             <CardContent>
               <h1>CADASTRO</h1>
-              <div className='card2'>
-                <Grid container rowSpacing={4} justifyContent="center" alignItems="center">
-                  <Grid size={5}>
+              <div className="card2">
+                <Grid
+                  container
+                  spacing={2}
+                  justifyContent="center"
+                  alignItems="center"
+                >
+                  <Grid item xs={12}>
                     <label>Nome</label>
-                    <input type='text' placeholder='Nome' className='input'/>
-                  </Grid>  
-                  <Grid size={5}>
-                    <label>CPF</label><br/>
-                    <input type='text' placeholder='CPF' className='input'/>
-                  </Grid>  
-                  <Grid size={5}>
-                    <label>E-mail</label>
-                    <input type='text' placeholder='E-mail' className='input'/>
+                    <input
+                      type="text"
+                      placeholder="Nome"
+                      className="input"
+                      value={nameInput}
+                      onChange={(e) => setNameInput(e.target.value)}
+                    />
                   </Grid>
-                  <Grid size={5}>
+                  <Grid item xs={12}>
+                    <label>CPF</label>
+                    <input
+                      type="text"
+                      placeholder="CPF"
+                      className="input"
+                      value={cpfInput}
+                      onChange={(e) => setCpfInput(e.target.value)}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <label>E-mail</label>
+                    <input
+                      type="text"
+                      placeholder="E-mail"
+                      className="input"
+                      value={registerEmailInput}
+                      onChange={(e) => setRegisterEmailInput(e.target.value)}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
                     <label>Senha</label>
-                    <input type='text' placeholder='Senha' className='input'/>
+                    <input
+                      type="password"
+                      placeholder="Senha"
+                      className="input"
+                      value={registerPasswordInput}
+                      onChange={(e) => setRegisterPasswordInput(e.target.value)}
+                    />
                   </Grid>
                 </Grid>
               </div>
-            </CardContent><br/>
-            <Button variant='contained' color='secondary'>Cadastrar</Button>
+            </CardContent>
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={handleRegister}
+            >
+              Cadastrar
+            </Button>
           </Card>
         </Grid>
       </Grid>
