9dd144094c26d0bf3cbe28d242cb9863afa2bbdc
ThaliaLevy <thalialevy@hotmail.com>
2024-11-14 22:22:05 -0300

    ultimos ajustes backend

--- backend/app.py
+++ backend/app.py
@@ -2,8 +2,10 @@ from flask import Flask
 from .routes.Company import company_bp
 from .routes.Volunteer import volunteer_bp
 from .routes.Vacancy import vacancy_bp
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}}, methods=["POST", "GET", "PUT", "DEL", "OPTIONS"])
 
 app.register_blueprint(vacancy_bp)
 app.register_blueprint(company_bp)
